6ce59faffdc3e8c8f0a96a8c8f26b3f9de404a19
Nishant Gupta <nishantgpt46@gmail.com>
2024-11-05 14:55:35 +0200

    Final Project structure

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 import os
-from src.utilis import save_object
+from src.utils import save_object
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
